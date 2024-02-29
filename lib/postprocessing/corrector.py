f3b161160b5851a644ffbe9143717437d88b1492
SiddarthNarayanan01 <siddarth.r.narayanan@gmail.com>
2024-02-28 22:03:35 -0500

    commit before working on evolution

--- lib/postprocessing/corrector.py
+++ lib/postprocessing/corrector.py
@@ -1,19 +1,25 @@
+from typing import Literal
+from lib.sampling.models import Models
 from lib.sampling.sampler import Sampler
-from lib.sampling.backend import Backend
 from lib.postprocessing.parser import RegExParser
 
 
-
 class Corrector(Sampler):
-    def __init__(self, backend: Backend, prompt_supplement: str = "") -> None:
+    def __init__(
+        self,
+        backend: Literal["llamacpp", "groq", "ollama"],
+        model_name: Models,
+        prompt_supplement: str = "",
+    ) -> None:
         if not prompt_supplement:
             prompt_supplement = """Correct any syntax errors that are present in this code.
-            Do NOT change the intent of the code, simply fix syntactical issues and/or issues with numpy, itertools, and any other library,
-            that may have been made. Once you are done fixing the code, simply output the fixed Python function in markdown format.
-            Use markdown code blocks to separate the code from any explanations or commentary that you provide."""
-        super().__init__(backend, prompt_supplement)
+            Do NOT change the intent/underlying workings of the code. Simply fix syntactical issues and/or issues with numpy, itertools,
+            and any other imported library, that may have been made. Once you are done fixing the code, simply output the fixed Python 
+            function in markdown format. Use a markdown code block to separate the revised function from any explanations or commentary that you provide."""
+        super().__init__(backend, model_name)
 
     def fix(self, sample: str) -> str:
         return RegExParser.parse(
             self.backend.prompt(f"{self.prompt_supplement}\n\n{sample}")
         )
+
--- test.py
+++ test.py
@@ -1,14 +1,45 @@
+import json
 from lib.sampling.models import Models
 from lib.sampling.sampler import Sampler
+from threading import Thread
+import requests
+import time
+import multiprocessing
 
 
 sampler = Sampler(
-    backend="ollama",
+    backend="llamacpp",
     model_name=Models.DEEPSEEK_MATH,
-    ollama_server_address="http://127.0.0.1:11434"
+    model_weights_path="./model_weights/deepseek-math-7b-rl.Q8_0.gguf",
 )
 
 prompt = r"""
+Hello! Who are you?
 """
 
-sampler.sample(prompt, print)
+
+# def process(prompt):
+#     print(time.time())
+#     data = {"prompt": prompt, "n_predicts": 100}
+#     r = requests.post("http://localhost:8080/completion", data=json.dumps(data))
+#     print(r.json()["content"])
+#     print("\n" * 10)
+#
+#
+# if __name__ == "__main__":
+#     pool = multiprocessing.Pool(processes=8)
+#     pool.map(process, [prompt for _ in range(8)])
+
+def sample(sampler, prompt, callback):
+    sampler.sample(prompt, callback)
+
+threads = [
+    Thread(target=sample, args=(sampler, prompt, print)),
+    Thread(target=sample, args=(sampler, prompt, print)),
+    Thread(target=sample, args=(sampler, prompt, print)),
+    Thread(target=sample, args=(sampler, prompt, print)),
+]
+
+for i, t in enumerate(threads):
+    t.start()
+    t.join()
