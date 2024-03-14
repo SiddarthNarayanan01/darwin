aea09f6cc592bb2fcbcd89f51b3409477e20bb72
Summma <pinto.alex05@gmail.com>
2024-03-14 00:19:52 -0400

    added clusters (also no bug testing lol, tired)

--- lib/evolving/clusters.py
+++ lib/evolving/clusters.py
@@ -1,2 +1,21 @@
+import numpy as np
+from scipy.special import softmax
+
+from lib.evolving.samples import Sample
+
+
 class Cluster:
-    pass
+    def __init__(self, sample: Sample) -> None:
+        self.score: int = sample.score
+        self.samples: list[Sample] = sample
+        self.lengths: list[int] = [len(sample.program)]
+
+    def register_sample(self, sample: Sample) -> None:
+        self.samples.append(sample)
+        self.lengths.append(len(sample.program))
+
+    def get_sample(self) -> Sample:
+        normalized = (np.array(self.lengths) - min(self.lengths)) / (
+            max(self.lengths) + 1e-6
+        )
+        return np.random.choice(self.samples, p=softmax(-normalized))
