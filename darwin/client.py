421d05c1685e12591f448c4d9be41c1ddad42e2a
SiddarthNarayanan01 <siddarth.r.narayanan@gmail.com>
2024-04-19 14:18:18 -0400

    added many todo comments to track progress

--- darwin/client.py
+++ darwin/client.py
@@ -27,6 +27,9 @@ class DarwinClient:
         self.evaluators: List[Tuple[str, int]] = []
         self.processes = []
 
+
+    # TODO: Simplify start evolution process
+    # Make more modular / distribute across more functions
     def start_evolution(self):
         # Create samplers
         self.spawn_samplers()
@@ -34,17 +37,21 @@ class DarwinClient:
         self.spawn_postprocessors()
         # Create evaluators
         for i in range(self.config.eval.n_evaluators):
+            # TODO: Pass optional kwargs from eval config to eval server
+            # TODO: Figure out how to spawn them on localhost (avoid conflicts)
+            # Avoid conflicts, configurable in respective config objects. Same applies
+            # to other server types
+            # If you are wondering how to pass kwargs -> **kwargs as last paramater
             s = EvaluationServer("127.0.0.1", 14232 + i, self.solve_function)
             self.evaluators.append(("127.0.0.1", 14232 + i))
             self.start_server(s)
 
         # Create the evovler
+        # TODO: Pass optional kwargs from evolve config to evolve server
         evolver = EvolverServer(
-            "127.0.0.1",
-            11111,
-            self.config.evolve,
-            self.evolve_function
+            "127.0.0.1", 11111, self.config.evolve, self.evolve_function
         )
+        # TODO: Actually implement the task queue / evolution logic with the task classes from taskmaster
 
     def spawn_samplers(self):
         md = self.config.sample.model_distribution
@@ -56,6 +63,7 @@ class DarwinClient:
                 n = floor(self.config.sample.n_samplers * md[m])
             samplers_left -= n
             for i in range(n):
+                # TODO: Pass optional kwargs from sampler config to sampler server
                 s = SamplerServer("127.0.0.1", 12345 + i, self.config.sample.backend, m)
                 self.samplers.append(("127.0.0.1", 12345 + i))
                 self.start_server(s)
@@ -63,6 +71,7 @@ class DarwinClient:
     def spawn_postprocessors(self):
         if not self.config.postprocess.model_distribution:
             for i in range(self.config.postprocess.n_postprocessors):
+                # TODO: Pass optional kwargs from postprocessors config to postprocessors server
                 s = PostProcessorServer(
                     "127.0.0.1",
                     9147 + i,
@@ -71,6 +80,7 @@ class DarwinClient:
                 self.postprocessors.append(("127.0.0.1", 9147 + i))
             return
 
+        # TODO: Is there a better way of spawning servers that have a model_distribution property?
         md = self.config.postprocess.model_distribution
         pp_left = self.config.postprocess.n_postprocessors
         for i, m in enumerate(sorted(list(md.keys()), key=lambda x: md[x])):
@@ -80,6 +90,7 @@ class DarwinClient:
                 n = floor(self.config.postprocess.n_postprocessors * md[m])
             pp_left -= n
             for i in range(n):
+                # TODO: Pass optional kwargs from postprocessors config to postprocessors server
                 s = PostProcessorServer(
                     "127.0.0.1",
                     9147 + i,
@@ -90,6 +101,7 @@ class DarwinClient:
                 self.postprocessors.append(("127.0.0.1", 9147))
                 self.start_server(s)
 
+    # TODO: We should also handle graceful termination of servers (self.app.shutdown() in server)
     def start_server(self, s: Server):
         process = Process(target=s.start_server)
         process.start()
