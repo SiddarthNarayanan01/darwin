aa58a5adf50f3a83bd113ed73049c39c50f43697
SiddarthNarayanan01 <siddarth.r.narayanan@gmail.com>
2024-04-26 23:42:20 -0400

    edited test files

--- test2.py
+++ test2.py
@@ -1,18 +1,24 @@
-from darwin import *
+from darwin2.client.ollama import OllamaClient
+from darwin2.configuration.ollama import OllamaConfig
 
-spec = open("myspec", "r")
-
-config = OllamaClientConfig(
-    n_samplers=24,
-    samplers={"deepseek-coder:6.7b-instruct": 0.5, "gemma:7b": 0.5},
+config = OllamaConfig(
+    samplers={"deepseek-coder:6.7b-instruct": 0.5, "llama3:8b": 0.5},
+    n_samplers=6,
+    correctors={"llama2:latest": 1},
+    n_correctors=2,
     n_evaluators=10,
-    n_post_processors=5,
-    post_processors={"deepseek-coder:6.7b-instruct": 0.5, "gemma:7b": 0.5} | None = None,
 )
 
-client = OllamaClient(
-    ollama_addresses=["http://localhost:11434", "www.myserver.com"], config=config
-)
+# To access endpoint on different computer, make sure to specify OLLAMA_HOST=IP:PORT
+client = OllamaClient(endpoints=["http://localhost:11434/api/generate"], corrector_endpoints=["http://172.31.142.130:11434/api/generate", "http://172.31.133.234:11434/api/generate"], config=config)
 
-client.start(spec.read(), evolve_function="priority", eval_function="evaluate")
+with open("./sentence_specification.py", "r") as f:
+    spec = f.read()
 
+if __name__ == "__main__":
+    client.start(
+        spec,
+        evolve_function_name="priority",
+        solve_function_name="evaluate",
+        inputs=(5, 3, 3),
+    )
